505eaf16daec10b85a82542ea2e4a70a8103fd0b
Angelo Cisneros <a.e.cisneros@gmail.com>
2015-01-10 12:45:23 -0800

    Add validations to Articles for Title and Content

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ActiveRecord::Base
   # validations
+  # validates :user_id, presence: true
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :content, presence: true, length: { maximum: 140 }
+  
   # associations
   belongs_to :user
 end
